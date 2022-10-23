0a293663bd059fb3e2d7d65fb0fa4db49e1a7de3
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-10-22 22:44:29 -0300

    first commit

---
+++ lib/flight_booking1.ex
@@ -0,0 +1,18 @@
+defmodule FlightBooking1 do
+  @moduledoc """
+  Documentation for `FlightBooking1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlightBooking1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
