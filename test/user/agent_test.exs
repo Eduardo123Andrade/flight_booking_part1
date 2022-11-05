30f7e1bf11ce2ad07f7ee383687f86110f6e2536
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-05 19:18:10 -0300

    created agent_test

---
+++ test/user/agent_test.exs
@@ -0,0 +1,49 @@
+defmodule FlightBooking1.User.AgentTest do
+  use ExUnit.Case
+
+  import FlightBooking1.Factory
+
+  alias FlightBooking1.Users.Agent, as: UserAgent
+  # alias FlightBooking1.Users.User
+
+  describe "save/1" do
+    test "saves the user" do
+      user = build(:user)
+
+      UserAgent.start_link(%{})
+
+      assert UserAgent.save(user) == :ok
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      UserAgent.start_link()
+      :ok
+    end
+
+    test "when the user is found, return the user" do
+      user = build(:user)
+
+      UserAgent.save(user)
+
+      response = UserAgent.get(user.id)
+      expected_response = {:ok, user}
+
+      assert response == expected_response
+    end
+
+    test "when the user is not found, return an error" do
+      wrong_id = UUID.uuid4()
+
+      :user
+      |> build
+      |> UserAgent.save()
+
+      response = UserAgent.get(wrong_id)
+      expected_response = {:error, "User not found"}
+
+      assert response == expected_response
+    end
+  end
+end
