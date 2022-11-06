f2366332134b8b788ff9c11ada683689d5a8585c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-05 22:40:13 -0300

    refactored booking

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -5,23 +5,27 @@ defmodule FlightBooking1.Bookings.Booking do
   defstruct @keys
 
   def build(user_id, local_origin, local_destination, complete_date) do
+    uuid = UUID.uuid4()
     local_date = NaiveDateTime.local_now()
 
-    cond do
-      NaiveDateTime.compare(complete_date, local_date) != :gt ->
-        {:error, "Invalid date"}
+    compare = NaiveDateTime.compare(complete_date, local_date)
 
-      NaiveDateTime.compare(complete_date, local_date) == :gt ->
-        uuid = UUID.uuid4()
+    create_booking(uuid, user_id, local_origin, local_destination, complete_date, compare)
+  end
 
-        {:ok,
-         %__MODULE__{
-           id: uuid,
-           complete_date: complete_date,
-           local_origin: local_origin,
-           local_destination: local_destination,
-           user_id: user_id
-         }}
-    end
+  defp create_booking(id, user_id, local_origin, local_destination, complete_date, compare)
+       when compare == :gt do
+    {:ok,
+     %__MODULE__{
+       id: id,
+       complete_date: complete_date,
+       local_origin: local_origin,
+       local_destination: local_destination,
+       user_id: user_id
+     }}
+  end
+
+  defp create_booking(_id, _user_id, _local_origin, _local_destination, _complete_date, _compare) do
+    {:error, "Invalid date"}
   end
 end
