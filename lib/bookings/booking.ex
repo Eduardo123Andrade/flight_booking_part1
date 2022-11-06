d894b14ad12af4814071390e049eb880d5fbace1
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-05 22:05:37 -0300

    created error case in booking

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -5,15 +5,23 @@ defmodule FlightBooking1.Bookings.Booking do
   defstruct @keys
 
   def build(user_id, local_origin, local_destination, complete_date) do
-    uuid = UUID.uuid4()
+    local_date = NaiveDateTime.local_now()
 
-    {:ok,
-     %__MODULE__{
-       id: uuid,
-       complete_date: complete_date,
-       local_origin: local_origin,
-       local_destination: local_destination,
-       user_id: user_id
-     }}
+    cond do
+      NaiveDateTime.compare(complete_date, local_date) != :gt ->
+        {:error, "Invalid date"}
+
+      NaiveDateTime.compare(complete_date, local_date) == :gt ->
+        uuid = UUID.uuid4()
+
+        {:ok,
+         %__MODULE__{
+           id: uuid,
+           complete_date: complete_date,
+           local_origin: local_origin,
+           local_destination: local_destination,
+           user_id: user_id
+         }}
+    end
   end
 end
